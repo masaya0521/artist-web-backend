bc2ab6f0ee8a4ecfc0fb3af142b4e68afa9aa365
masaya0521 <kaji0521km@gmail.com>
2024-03-16 20:48:02 +0900

    Add Axum and Tokio dependencies, implement basic routing and response handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get, response::Html};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hendler));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .unwrap();
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn hendler() -> Html<&'static str> {
+    Html("<h1>Hello, World!</h1>")
 }
